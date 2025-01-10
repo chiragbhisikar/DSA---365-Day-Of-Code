efb05316ab636c82217b4a18eb512c8e5ad03c39
chiragbhisikar <developerxchirag@gmail.com>
2025-01-10 22:36:31 +0530

    Sorting Complete In Striver

---
+++ L-43 4 Pillars of OOPs Concept -Inheritance, Polymorphism, Encapsulation & Abstraction/Encapsulation.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Student
+{
+private:
+    string name;
+    int height;
+    int age;
+
+public:
+    int getAge()
+    {
+        return this->age;
+    }
+};
+
+int main()
+{
+    Student s;
+    return 0;
+}
